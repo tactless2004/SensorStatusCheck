ff5c9b3450056b582698eed0c78e94968d46d348
tactless2004 <leyton.mckinney@gmail.com>
2024-06-26 21:44:26 -0500

    added CO2 check

--- run_check.py
+++ run_check.py
@@ -1,2 +1,23 @@
 #### Sensor Status Check
+from colorama import Fore
+from board import I2C
+from adafruit_scd4x import SCD4X
 
+
+def start():
+    try:
+        i2c = I2C()
+        co2_temp_humidity_sensor = SCD4X(i2c)
+        co2, temp, humidity = (
+            co2_temp_humidity_sensor.CO2,
+            co2_temp_humidity_sensor.temperature,
+            co2_temp_humidity_sensor.relative_humidity,
+        )
+        print(f"{Fore.GREEN}SUCCESS{Fore.RESET}")
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    start()
